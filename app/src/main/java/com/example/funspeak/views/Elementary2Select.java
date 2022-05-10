58a1067b8ee1906f08efacd2641e7c532523d1a7
AlexPalmaP12 <alexpalmap12@gmail.com>
2022-05-10 02:47:16 -0500

    Revert "Revert "Update 2?""
    This reverts commit 177d1cecfdf5fdbb4c03edf0be8efff095c5aa80.

---
+++ app/src/main/java/com/example/funspeak/views/Elementary2Select.java
@@ -0,0 +1,38 @@
+package com.example.funspeak.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.funspeak.R;
+
+public class Elementary2Select extends AppCompatActivity {
+
+    private Button test, study, back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_elementary2_select);
+        bindComponents();
+        setUpActions();
+
+        //language = getIntent().getIntExtra("language",0);
+    }
+
+    public void bindComponents(){
+        test = findViewById(R.id.subtraction2);
+        study = findViewById(R.id.addition2);
+        back = findViewById(R.id.back);
+    }
+
+    public void setUpActions(){
+
+        study.setOnClickListener((view)->{
+            startActivity(new Intent(this, Elementary2.class));
+        });
+
+    }
+}
